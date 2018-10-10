00c58324f1af46bd382e9c8d8320926010f5970c
Yun Park <hanlsin@gmail.com>
2018-10-10 12:41:25 -0700

    add test function templates for account

--- tests/test_account.py
+++ tests/test_account.py
@@ -12,3 +12,32 @@ from herapy.transaction import transaction
 #def test_sign_transaction():
 #    account = herapy.account.Account()
 #    account.sign_transaction( transaction.Transaction("") )
+
+def test_create_new_account():
+    # TODO fill this function
+    # check a result with 'aergocli'
+    # 1. get password
+    # 2. make a private key
+    # 3. get a public key
+    # 4. get an address
+    pass
+
+def test_import_accounts():
+    # TODO fill this function
+    # 1. export accounts using 'aergocli'
+    # 2. import exported file
+    # 3. get accounts
+    # 4. extract a private key
+    # 5. generate a public key and address
+    # 6. compare the public key and address from 'aergocli'
+    pass
+
+def test_lock_n_unlock_accounts():
+    # TODO fill this function
+    # 1. export accounts using 'aergocli'
+    # 2. import exported file
+    # 3. get accounts
+    # 4. lock an account and check status
+    # 5. unlock the account and check status
+    pass
+
