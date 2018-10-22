a9e838c0b329f5cddcfabddff9d30bb33c42b5b2
Yun Park <hanlsin@gmail.com>
2018-10-22 13:42:41 +0900

    expose Account class

--- herapy/__init__.py
+++ herapy/__init__.py
@@ -6,6 +6,7 @@ __author__ = """Yun Park"""
 __email__ = 'hanlsin@gmail.com'
 __version__ = '0.1.0'
 
-__all__ = ["aergo"]
+__all__ = ["aergo", "account"]
 
 from .aergo import Aergo
+from .account import Account
