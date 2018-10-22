732e4be9c6ece90e876e6082c4390987dd7d4088
Yun Park <hanlsin@gmail.com>
2018-10-22 15:56:56 +0900

    delete unnecessary files

--- herapy/blockchain/blockchain.py
+++
@@ -1,9 +0,0 @@
-def is_connected():
-    pass
-
-def best_block():
-    pass
-
-# def get_block(hash):
-#     if len(hash) !== 64:
-#         raise InvalidBlockError('Invalid block hash. Must be 32 byte encoded in hex. Did you mean to pass a block number?')
