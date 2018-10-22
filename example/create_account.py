69a7dfc69f0974c04c69b29d6452167ff6be3484
Yun Park <hanlsin@gmail.com>
2018-10-22 13:45:40 +0900

    change and add example to handle account and account's state

--- example/create_account.py
+++ example/create_account.py
@@ -10,24 +10,61 @@ def run():
         print("------ Connect AERGO -----------")
         aergo.connect('localhost:7845')
 
+        print("------ Get Accounts in Node -----------")
+        accounts = aergo.get_node_accounts()
+        for i in range(len(accounts)):
+            print("Account[{0}]'s address in Node is {1}".format(i, accounts[i].address_str))
+            state = aergo.get_account_state(accounts[i])
+            print("  > account state : {}".format(state))
+            print("    - balance        = {}".format(accounts[i].balance))
+            print("    - nonce          = {}".format(accounts[i].nonce))
+            print("    - code hash      = {}".format(accounts[i].code_hash))
+            print("    - storage root   = {}".format(accounts[i].storage_root))
+
         print("------ Create Account -----------")
         password = "test password"
-        account = aergo.create_account(password=password)
+        account = aergo.new_account(password=password)
 
         print("Private Key      = {}".format(account.private_key))
+        print("str(Private Key) = {}".format(account.private_key_str))
         print("Public Key       = {}".format(account.public_key))
         print("Address          = {}".format(account.address))
-        print("Address String   = {}".format(account.address_str))
+        print("str(Address)     = {}".format(account.address_str))
 
         print("------ Get Account State -----------")
-        result = aergo.get_account_state(account)
-        print('Return Msg: %s' % result)
-        '''
-        print('  - Nonce:        %s' % result.nonce)
-        print('  - Balance:      %s' % result.balance)
-        print('  - Code Hash:    %s' % result.codeHash)
-        print('  - Storage Root: %s' % result.storageRoot)
-        '''
+        state = aergo.get_account_state()
+        print("  > account state : {}".format(state))
+        print('    - Nonce:        %s' % account.nonce)
+        print('    - Balance:      %s' % account.balance)
+        print('    - Code Hash:    %s' % account.code_hash)
+        print('    - Storage Root: %s' % account.storage_root)
+
+        print("------ Get Configured Account -----------")
+        conf_keys_str = """
+1:6hbRWgddqcg2ZHE5NipM1xgwBDAKqLnCKhGvADWrWE18xAbX8sW:AmPESicKLcPYXJC7ufgK6ti3fVS1r1SbqfxhVDEnTUc5cPXT1474
+"""
+        conf_keys_line = conf_keys_str.split('\n')
+        conf_keys = []
+        for l in conf_keys_line:
+            if 0 == len(l):
+                continue
+            conf_keys.append(l.split(':'))
+        print("  - Configured Keys:\n{}".format(conf_keys))
+
+        for k in conf_keys:
+            print("  [{}]".format(k[0]))
+            print("    > private key   : {}".format(k[1]))
+            print("    > address       : {}".format(k[2]))
+
+            # check account state
+            a = herapy.Account("", empty=True)
+            a.address = k[2]
+            state = aergo.get_account_state(a)
+            print("    > account state : {}".format(state))
+            print("      - balance        = {}".format(a.balance))
+            print("      - nonce          = {}".format(a.nonce))
+            print("      - code hash      = {}".format(a.code_hash))
+            print("      - storage root   = {}".format(a.storage_root))
 
         print("------ Disconnect AERGO -----------")
         aergo.disconnect()
