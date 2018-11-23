accf7e6966bb1c80bc87499398357a4409577872
Yun Park <hanlsin@gmail.com>
2018-11-23 20:32:06 +0900

    upgrade patch version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = ['pytest', ]
 
 setup(
     name='aergo-herapy',
-    version='0.1.12',
+    version='0.1.13',
     description="python SDK for AERGO",
     keywords='herapy',
     author="aergo.io",
