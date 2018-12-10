1b104a172da382bc2056fb8e7836b609307a7c2b
Yun Park <hanlsin@gmail.com>
2018-12-10 02:59:47 +0000

    lint refactoring for a consistent function usage

--- aergo/herapy/utils/converter.py
+++ aergo/herapy/utils/converter.py
@@ -6,7 +6,7 @@ import json
 import base58
 
 from ..grpc import blockchain_pb2
-from .encoding import encode_address, encode_tx_hash
+from .encoding import encode_address
 
 
 def convert_tx_to_grpc_tx(tx):
@@ -28,6 +28,10 @@ def convert_tx_to_grpc_tx(tx):
     if tx.sign is not None:
         grpc_tx.body.sign = tx.sign
     return grpc_tx
+
+
+def tx_to_grpc_tx(v):
+    return convert_tx_to_grpc_tx(v)
 
 
 def convert_tx_to_json(tx):
@@ -59,19 +63,34 @@ def convert_tx_to_json(tx):
     return json_tx
 
 
+def tx_to_json(v):
+    return convert_tx_to_json(v)
+
+
 def convert_tx_to_formatted_json(tx):
     if tx is None:
         return None
-
     return json.dumps(convert_tx_to_json(tx), indent=2)
+
+
+def tx_to_formatted_json(v):
+    return convert_tx_to_formatted_json(v)
 
 
 def convert_bytes_to_int_str(v):
     return ''.join('{:d} '.format(x) for x in v)
 
 
+def bytes_to_int_str(v):
+    return convert_bytes_to_int_str(v)
+
+
 def convert_bytes_to_hex_str(v):
     return ''.join('0x{:02x} '.format(x) for x in v)
+
+
+def bytes_to_hex_str(v):
+    return convert_bytes_to_hex_str(v)
 
 
 def convert_luajson_to_json(v):
@@ -79,7 +98,17 @@ def convert_luajson_to_json(v):
     v = v[1:len(v)-1]
     return json.loads(v)
 
-def bigint_to_bytes(number):
+
+def luajson_to_json(v):
+    return convert_luajson_to_json(v)
+
+
+def convert_bigint_to_bytes(number):
     q, r = divmod(len(bin(number))-2, 8)
     bytes_to_fit_number = q if r == 0 else q + 1
     return number.to_bytes(bytes_to_fit_number, 'big')
+
+
+def bigint_to_bytes(v):
+    return convert_bigint_to_bytes(v)
+
