35b5471020acf3ded954178c63344d46bbf1f2e2
Yun Park <hanlsin@gmail.com>
2018-12-12 20:41:21 +0000

    version up to fix cannot find the 'grpc' module

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = ['pytest', ]
 
 setup(
     name='aergo-herapy',
-    version='0.1.20',
+    version='0.1.21',
     description="python SDK for AERGO",
     keywords='herapy',
     author="aergo.io",
