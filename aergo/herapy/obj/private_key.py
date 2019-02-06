b805fc2a87fa0f52e1f9c70633b804b2ca9b345b
Yun Park <hanlsin@gmail.com>
2019-02-06 13:46:48 +0000

    update private key logic to avoid assertion error

--- aergo/herapy/obj/private_key.py
+++ aergo/herapy/obj/private_key.py
@@ -6,7 +6,6 @@ import hashlib
 from ecdsa.util import number_to_string, string_to_number
 
 from ..utils.encoding import encode_private_key, decode_private_key
-from ..constants import PRIVATE_KEY_BYTES_LENGTH
 
 
 class PrivateKey:
@@ -27,20 +26,38 @@ class PrivateKey:
                                                        hashfunc=hashlib.sha256)
         self.__private_key = self.__signing_key.privkey
 
+    def __get_singing_key_instance(self, private_key):
+        d = int.from_bytes(private_key, byteorder='big')
+        """
+        sk = ecdsa.SigningKey.from_secret_exponent(secexp=d,
+                                                   curve=ecdsa.SECP256k1,
+                                                   hashfunc=hashlib.sha256)
+        """
+        # to avoid 'Assert' error
+        sk = ecdsa.SigningKey(_error__please_use_generate=True)
+        sk.curve = ecdsa.SECP256k1
+        sk.default_hashfunc = hashlib.sha256
+        sk.baselen = ecdsa.SECP256k1.baselen
+        pubkey_point = ecdsa.SECP256k1.generator * d
+        pubkey = ecdsa.ecdsa.Public_key(ecdsa.SECP256k1.generator, pubkey_point)
+        pubkey.order = ecdsa.SECP256k1.order
+        sk.verifying_key = ecdsa.VerifyingKey.from_public_point(pubkey_point,
+                                                                ecdsa.SECP256k1,
+                                                                hashlib.sha256)
+        sk.privkey = ecdsa.ecdsa.Private_key(pubkey, d)
+        sk.privkey.order = ecdsa.SECP256k1.order
+        return sk
+
     def __get_key(self, private_key):
         if isinstance(private_key, str):
             private_key = decode_private_key(private_key)
 
-        d = int.from_bytes(private_key, byteorder='big')
-        sk = ecdsa.SigningKey.from_secret_exponent(secexp=d,
-                                                   curve=ecdsa.SECP256k1,
-                                                   hashfunc=hashlib.sha256)
-        self.__signing_key = sk
+        self.__signing_key = self.__get_singing_key_instance(private_key)
         self.__private_key = self.__signing_key.privkey
 
     def __get_private_key_bytes(self):
         d = self.__private_key.secret_multiplier
-        return d.to_bytes(PRIVATE_KEY_BYTES_LENGTH, byteorder='big')
+        return d.to_bytes((d.bit_length() + 7) // 8, byteorder='big')
 
     def __canonicalize_int(self, n, order):
         b = number_to_string(n, order)
