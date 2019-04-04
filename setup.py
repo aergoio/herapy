226e71eef8126477c0423f8c621c9463bbee007d
Yun Park <hanlsin@gmail.com>
2019-04-04 14:30:58 +0100

    release v1.0.3

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ test_requirements = [
 
 setup(
     name='aergo-herapy',
-    version='1.0.2',
+    version='1.0.3',
     description="python SDK for AERGO",
     keywords='herapy',
     author="aergo.io",
