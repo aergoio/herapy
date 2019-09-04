9dbe2357bc87afd70dc613c6a88972357e9d4b32
Yun Park <hanlsin@gmail.com>
2019-09-04 16:04:02 +0900

    encode owner and destination property of name info

--- aergo/herapy/obj/name_info.py
+++ aergo/herapy/obj/name_info.py
@@ -4,12 +4,16 @@ import json
 
 from google.protobuf.json_format import MessageToJson
 
+from ..utils.encoding import encode_address
+
 
 class NameInfo():
     """ NameInfo is used to store information of name system."""
     def __init__(self, info):
         self.__info = json.loads(MessageToJson(info))
-        self.__name = self.__info['name']['name']
+        self.__name = info.name.name
+        self.__owner = info.owner
+        self.__dest = info.destination
 
     @property
     def info(self):
@@ -19,10 +23,30 @@ class NameInfo():
     def name(self):
         return self.__name
 
+    @property
+    def owner(self):
+        return self.__owner
+
+    @property
+    def destination(self):
+        return self.__dest
+
     def json(self):
-        rt = self.__info
-        rt['name'] = self.name
-        return rt
+        if self.owner is not None:
+            owner = encode_address(self.owner)
+        else:
+            owner = ""
+
+        if self.destination is not None:
+            dest = encode_address(self.destination)
+        else:
+            dest = ""
+
+        return {
+            'name': self.name,
+            'owner': owner,
+            'destination': dest
+        }
 
     def __str__(self):
         return json.dumps(self.json(), indent=2)
