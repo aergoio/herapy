6de716344a927b1f71c05f68858918f7b5b2db89
Yun Park <hanlsin@gmail.com>
2019-09-04 15:53:55 +0900

    change return json display for name info

--- aergo/herapy/obj/name_info.py
+++ aergo/herapy/obj/name_info.py
@@ -9,9 +9,20 @@ class NameInfo():
     """ NameInfo is used to store information of name system."""
     def __init__(self, info):
         self.__info = json.loads(MessageToJson(info))
+        self.__name = self.__info['name']['name']
+
+    @property
+    def info(self):
+        return self.__info
+
+    @property
+    def name(self):
+        return self.__name
 
     def json(self):
-        return self.__info
+        rt = self.__info
+        rt['name'] = self.name
+        return rt
 
     def __str__(self):
         return json.dumps(self.json(), indent=2)
