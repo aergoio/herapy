da74722573b21f914f73ea500036c0fe3d48d997
Pierre-Alain <ouvrard.pierre.alain@gmail.com>
2020-09-23 11:38:54 +0900

    release v2.0.2

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ test_requirements = [
 
 setup(
     name='aergo-herapy',
-    version='2.0.1',
+    version='2.0.2',
     description="python SDK for AERGO",
     keywords='herapy',
     author="aergo.io",
